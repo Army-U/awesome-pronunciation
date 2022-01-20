d61ead03498207b0b4e9c27954182254d3db9404
Jeremy Wu <jeremy.wu01@sap.com>
2022-01-20 09:50:34 +0800

    refactor: remove duplicate agile word

--- download.js
+++ download.js
@@ -1,67 +1,138 @@
-const fs = require('fs')
-const http = require('http')
-const https = require('https')
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
-const DB_NAME = './db.json'
-const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-const HTTP = 'http://'
-const HTTPS = 'https://'
+const DB_NAME = './db.json';
+const ALPHABET =
+	'ABCDEFGHIJKLMNOPQRSTUVWXYZ_';
+const HTTP = 'http://';
+const HTTPS = 'https://';
 
 /**
  * download audios
  */
 function download() {
-  const data = JSON.parse(fs.readFileSync(DB_NAME))
-  for(const i of ALPHABET) {
-    const wordList = data[i]
-    downloadLetter(wordList)
-  }
+	const data = JSON.parse(
+		fs.readFileSync(DB_NAME),
+	);
+	for (const i of ALPHABET) {
+		const wordList = data[i];
+		downloadLetter(wordList);
+	}
 }
 
 /**
  * download single letter audios
- * @param {List} wordList 
+ * @param {List} wordList
  */
-function downloadLetter(wordList) {
-  for(const j of wordList) {
-    for(const index in j.origin) {
-      let filepath = ''
-      if(j.origin.length === 1) {
-        filepath = `./docs/.vuepress/public/audio/${j.word.replace('.','dot-')}.mp3`
-      } else {
-        filepath = `./docs/.vuepress/public/audio/${j.word.replace('.','dot-')}-${index}.mp3`
-      }
-      
-      if(!isFileExist(filepath)) {
-        const file = fs.createWriteStream(filepath)
-        if(j.origin[index].includes(HTTP)) {
-          http.get(j.origin[index], function(response) {
-            response.pipe(file)
-            console.log(filepath + ' has downloaded!')
-          })
-        } else if(j.origin[index].includes(HTTPS)){
-          https.get(j.origin[index], function(response) {
-            response.pipe(file)
-            console.log(filepath + ' has downloaded!')
-          })
-        }
-      }
-    }
-  }
+function downloadLetter(
+	wordList,
+) {
+	for (const j of wordList) {
+		for (const index in j.origin) {
+			let filepath = '';
+			if (
+				j.origin.length === 1
+			) {
+				filepath = `./docs/.vuepress/public/audio/${j.word.replace(
+					'.',
+					'dot-',
+				)}.mp3`;
+			} else {
+				filepath = `./docs/.vuepress/public/audio/${j.word.replace(
+					'.',
+					'dot-',
+				)}-${index}.mp3`;
+			}
+
+			if (
+				!isFileExist(filepath)
+			) {
+				const file =
+					fs.createWriteStream(
+						filepath,
+					);
+				if (
+					j.origin[
+						index
+					].includes(HTTP)
+				) {
+					http
+						.get(
+							j.origin[index],
+							function (
+								response,
+							) {
+								response.pipe(
+									file,
+								);
+								console.log(
+									filepath +
+										' has downloaded!',
+								);
+							},
+						)
+						.on(
+							'error',
+							() => {
+								console.log(
+									j.origin[
+										index
+									],
+								);
+							},
+						);
+				} else if (
+					j.origin[
+						index
+					].includes(HTTPS)
+				) {
+					https
+						.get(
+							j.origin[index],
+							function (
+								response,
+							) {
+								response.pipe(
+									file,
+								);
+								console.log(
+									filepath +
+										' has downloaded!',
+								);
+							},
+						)
+						.on(
+							'error',
+							() => {
+								console.log(
+									j.origin[
+										index
+									],
+								);
+							},
+						);
+				}
+			}
+		}
+	}
 }
 
 /**
  * whether the file exists
- * @param {String} path 
+ * @param {String} path
  */
 function isFileExist(path) {
-  try{
-    fs.accessSync(path, fs.F_OK);
-  }catch(e){
-    return false;
-  }
-  return true;
+	try {
+		fs.accessSync(
+			path,
+			fs.F_OK,
+		);
+	} catch (e) {
+		return false;
+	}
+	return true;
 }
 
-const options = process.argv
-download()
+const options = process.argv;
+download();
