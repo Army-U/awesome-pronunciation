014861725955e5d8b73584d8d0efdfa3cda97d13
Army-U <jeremy.wu.real@gmail.com>
2022-01-22 16:32:08 +0800

    tweak

---
+++ add.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const prettier = require('prettier');
+
+const newWords = [];
+
+const db = JSON.parse(
+	fs
+		.readFileSync('./db.json')
+		.toString(),
+);
+
+newWords.forEach((word) => {
+	const initial =
+		word[0].toUpperCase();
+	db[initial].unshift({
+		word,
+		origin: [
+			`https://dict.youdao.com/dictvoice?audio=${word}&type=2`,
+		],
+		symbol: [''],
+		reference: '',
+	});
+});
+
+fs.writeFileSync(
+	'./db.json',
+	prettier.format(
+		JSON.stringify(db),
+		{
+			useTabs: true,
+			parser: 'json',
+			printWidth: 30,
+		},
+	),
+);
