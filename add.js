392217d33a82d223015a32b1d3d8f8fc01c1e241
i542961 <jeremy.wu01@sap.com>
2022-02-16 17:00:04 +0800

    add photics api

--- add.js
+++ add.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const prettier = require('prettier');
+const fetch = require('node-fetch');
 
 const newWords = [];
 
@@ -9,18 +10,30 @@ const db = JSON.parse(
 		.toString(),
 );
 
-newWords.forEach((word) => {
-	const initial =
-		word[0].toUpperCase();
-	db[initial].unshift({
-		word,
-		origin: [
-			`https://dict.youdao.com/dictvoice?audio=${word}&type=2`,
-		],
-		phonetic: [''],
-		reference: '',
-	});
-});
+newWords.forEach(
+	async (word) => {
+    let IPA = ''
+    try {
+      const data = await fetch(
+        `https://dict.youdao.com/jsonapi_s?doctype=json&jsonversion=4&q=${word}&le=en&t=6&client=web&keyfrom=webdict`,
+      ).then((res) =>
+        res.json(),
+      );
+      IPA = data.ec.word.usphone
+    }
+		
+		const initial =
+			word[0].toUpperCase();
+		db[initial].unshift({
+			word,
+			origin: [
+				`https://dict.youdao.com/dictvoice?audio=${word}&type=2`,
+			],
+			phonetic: [IPA],
+			reference: '',
+		});
+	},
+);
 
 fs.writeFileSync(
 	'./db.json',
