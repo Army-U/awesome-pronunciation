f7a19387988627885a39339eeffc3f14677ba4a1
Jeremy <jeremy.wu.real@gmail.com>
2022-08-14 09:41:03 +0800

    tweak

--- add.js
+++ add.js
@@ -2,25 +2,7 @@ const fs = require('fs');
 const prettier = require('prettier');
 const fetch = require('node-fetch');
 
-const newWords = [
-  'distinguish',
-  'monitor',
-  'leverage',
-  'conduct',
-  'sugar',
-  'blurry',
-  'loose',
-  'metric',
-  'Vite',
-  'variant',
-  'comment',
-  'project',
-  'component',
-  'preprocessors',
-  'retrospective',
-  'median',
-  'extension',
-];
+const newWords = [];
 
 const db = JSON.parse(fs.readFileSync('./db.json').toString());
 
